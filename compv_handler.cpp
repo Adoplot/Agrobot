05a147c30b5961cf4cd91d8247a8aa9d14610c0d
Pavel Maksimkin <pavulis97@gmail.com>
2024-11-17 20:54:55 +0200

    Refactoring (CompV): extracted methods from state machine

--- compv_handler.cpp
+++ compv_handler.cpp
@@ -28,6 +28,12 @@ static json jsonParse(const std::string* data);
 static CompV_Request_t getJsonRequest(const json* json);
 static Cartesian_Pos_t getJsonPos(const json* json);
 
+static void handleSetPositionRequest(const json& json);
+static void sendUnreachableResponse();
+static void sendCompleteResponse();
+static void sendInProgressResponse();
+static void sendRobotCommand(int command, const std::string& action_name);
+
 // Interface to set value of targetPos_worldFrame
 void CompV_SetTargetPosWorldFrame(Cartesian_Pos_t new_value) {
     targetPos_worldFrame = new_value;
@@ -55,22 +61,15 @@ Cartesian_Pos_t* Compv_GetTargetPosWorldFrame() {
 
 
 // Reads received message, parses json to request, handles the request
-void Compv_HandleCmd(const std::string* data){
-    //Get socket data for ENET1
+void Compv_HandleCmd(const std::string* data) {
+    // Get socket data
     sockfd_enet1 = Connection_GetSockfd(SOCKTYPE_ENET1);
     sockfd_compv = Connection_GetSockfd(SOCKTYPE_COMPV);
     sockaddr_enet1 = Connection_GetSockAddr(SOCKTYPE_ENET1);
-    char buf_cmd[2]{};
-    double distance2target{0};
-    bool orientation_reached{false};
 
-    // Declare jsons and strings to send later
-    json json_send{};
-    std::string string_send{};
-
+    // Parse JSON and get request type
     json json = jsonParse(data);
     CompV_Request_t request = getJsonRequest(&json);
-    Hyundai_Data_t *eePos_worldFrame = Connection_GetEePosWorldFrame();
 
     switch (request) {
         case REQ_INVALID:
@@ -78,76 +77,23 @@ void Compv_HandleCmd(const std::string* data){
             break;
 
         case REQ_SYNC_TARGETS:
-            cout << "Requested target syncronization" << endl;
-            //todo: check every entry of an array if it could be reached
-            //todo: and fill new json object with flags if could be reached
-            //todo: send json array back to CompV
+            cout << "Requested target synchronization" << endl;
             break;
 
-        case REQ_SET_POS: {
-            cout << "Initiating <Set Pos> sequence" << endl;
-            tcp_data_state = TCP_DATA_NEW_AVAILABLE;
-            targetPos_camFrame = getJsonPos(&json);
-            targetPos_worldFrame = Transform_ConvertFrameTarget2World(&targetPos_camFrame,
-                                                                        eePos_worldFrame,
-                                                                        SCISSORS_LENGTH);
-
-            if(!RobotAPI_TargetIsReachable(&targetPos_worldFrame)){
-                //todo: extract function
-                cout << "Answer to CompV: Unreachable" << endl;
-                json_send["request"] = COMPV_REQUEST_SET_POSITION;
-                json_send["status"] = COMPV_ANSW_UNREACHABLE;
-                string_send = json_send.dump();
-                Connection_SendTcp(sockfd_compv, &string_send);
-                return;
-            }
-
-            distance2target = Transform_CalcDistanceBetweenPoints(eePos_worldFrame,&targetPos_worldFrame);
-            orientation_reached = Transform_CompareOrientations(ORIENTATION_ACCURACY, eePos_worldFrame, &targetPos_worldFrame);
-
-
-            if ((distance2target <= POSITIONING_ACCURACY) && orientation_reached) {
-                //todo: extract function
-
-                cout << "RCM: pos and ori is reached" << endl;
-                cout << "Answer to CompV: Complete" << endl;
-
-                json_send["request"] = COMPV_REQUEST_SET_POSITION;
-                json_send["status"] = COMPV_ANSW_COMPLETE;
-                string_send = json_send.dump();
-                Connection_SendTcp(sockfd_compv, &string_send);
-            }
-            else {
-                //todo: extract function
-                json_send["request"] = COMPV_REQUEST_SET_POSITION;
-                json_send["status"] = COMPV_ANSW_IN_PROGRESS;
-                string_send = json_send.dump();
-                Connection_SendTcp(sockfd_compv, &string_send);
-            }
-
-            // ToDo: check if the rotation is complete?
-            //  need to work out an algorithm with compv team
+        case REQ_SET_POS:
+            handleSetPositionRequest(json);
             break;
-        }
+
         case REQ_RETURN_TO_BASE:
-            cout << "Initiating <Return To Base> sequence" << endl;
-            snprintf(buf_cmd,sizeof(buf_cmd),"%d", ENET_RETURN_TO_BASE);     //buffer should be null-terminated. sprintf ensures \0 at the end.
-            RobotAPI_SendCmd(sockfd_enet1, sockaddr_enet1, buf_cmd, sizeof(buf_cmd));
-            bzero(buf_cmd,sizeof(buf_cmd));
+            sendRobotCommand(ENET_RETURN_TO_BASE, "Return To Base");
             break;
 
         case REQ_CUT:
-            cout << "Initiating <Cut> sequence" << endl;
-            snprintf(buf_cmd,sizeof(buf_cmd),"%d",ENET_CUT);
-            RobotAPI_SendCmd(sockfd_enet1, sockaddr_enet1, buf_cmd, sizeof(buf_cmd));
-            bzero(buf_cmd,sizeof(buf_cmd));
+            sendRobotCommand(ENET_CUT, "Cut");
             break;
 
         case REQ_STORE:
-            cout << "Initiating <Store> sequence" << endl;
-            snprintf(buf_cmd,sizeof(buf_cmd),"%d",ENET_STORE);
-            RobotAPI_SendCmd(sockfd_enet1, sockaddr_enet1, buf_cmd, sizeof(buf_cmd));
-            bzero(buf_cmd,sizeof(buf_cmd));
+            sendRobotCommand(ENET_STORE, "Store");
             break;
 
         default:
@@ -171,6 +117,65 @@ static json jsonParse(const std::string* data) {
     return received_json;
 }
 
+static void handleSetPositionRequest(const json& json) {
+    cout << "Initiating <Set Pos> sequence" << endl;
+
+    Hyundai_Data_t *eePos_worldFrame = Connection_GetEePosWorldFrame();
+
+    tcp_data_state = TCP_DATA_NEW_AVAILABLE;
+    targetPos_camFrame = getJsonPos(&json);
+    targetPos_worldFrame = Transform_ConvertFrameTarget2World(&targetPos_camFrame,
+                                                              eePos_worldFrame,
+                                                              SCISSORS_LENGTH);
+
+    if (!RobotAPI_TargetIsReachable(&targetPos_worldFrame)) {
+        sendUnreachableResponse();
+        return;
+    }
+
+    double distance2target = Transform_CalcDistanceBetweenPoints(eePos_worldFrame, &targetPos_worldFrame);
+    bool orientation_reached = Transform_CompareOrientations(ORIENTATION_ACCURACY, eePos_worldFrame, &targetPos_worldFrame);
+
+    if ((distance2target <= POSITIONING_ACCURACY) && orientation_reached) {
+        sendCompleteResponse();
+    } else {
+        sendInProgressResponse();
+    }
+}
+
+static void sendUnreachableResponse() {
+    cout << "Answer to CompV: Unreachable" << endl;
+    json json_send;
+    json_send["request"] = COMPV_REQUEST_SET_POSITION;
+    json_send["status"] = COMPV_ANSW_UNREACHABLE;
+    std::string string_send = json_send.dump();
+    Connection_SendTcp(sockfd_compv, &string_send);
+}
+
+static void sendCompleteResponse() {
+    cout << "Answer to CompV: Complete" << endl;
+    json json_send;
+    json_send["request"] = COMPV_REQUEST_SET_POSITION;
+    json_send["status"] = COMPV_ANSW_COMPLETE;
+    std::string string_send = json_send.dump();
+    Connection_SendTcp(sockfd_compv, &string_send);
+}
+
+static void sendInProgressResponse() {
+    cout << "Answer to CompV: In Progress" << endl;
+    json json_send;
+    json_send["request"] = COMPV_REQUEST_SET_POSITION;
+    json_send["status"] = COMPV_ANSW_IN_PROGRESS;
+    std::string string_send = json_send.dump();
+    Connection_SendTcp(sockfd_compv, &string_send);
+}
+
+static void sendRobotCommand(int command, const std::string& action_name) {
+    cout << "Initiating <" << action_name << "> sequence" << endl;
+    char buf_cmd[2]{};
+    snprintf(buf_cmd, sizeof(buf_cmd), "%d", command);
+    RobotAPI_SendCmd(sockfd_enet1, sockaddr_enet1, buf_cmd, sizeof(buf_cmd));
+}
 
 // Gets request type from parsed JSON.
 // Returns: request type
